b6abed206f18c137a5b2129b356d7e33fd4df434
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-22 15:37:22 -0400

    the program now adds an extra cohort and also increases each cohort number by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -6,8 +6,13 @@ students = {
 
  def display(hash)#method returns hash as list
  	hash.each do |cohort, studentnum|
- 		puts "#{cohort}: #{studentnum}" 
+ 		five = studentnum * 0.05 #increase each cohort number by 5%
+ 		puts "#{cohort}: #{studentnum + five}" 
  	end
  end
 
 display(students)#calls method
+students[:cohort4]= 43
+display(students)#calls method with extra key and value
+puts students.keys
+display(students)
